e7830907df652205b07d3911f16b56acfcfeb4ce
henriquecscode <henriquecostasousa.hcs@gmail.com>
2026-01-03 11:17:42 +0000

    spring app init

---
+++ backend/fivault/src/main/java/com/fivault/fivault/FivaultApplication.java
@@ -0,0 +1,13 @@
+package com.fivault.fivault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FivaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FivaultApplication.class, args);
+	}
+
+}
---
+++ backend/fivault/src/test/java/com/fivault/fivault/FivaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fivault.fivault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FivaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
