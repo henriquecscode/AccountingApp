dc5bfb6a60b05b2836fd3732f04bb1d78108ae2a
henriquecscode <henriquecostasousa.hcs@gmail.com>
2026-01-03 16:17:13 +0000

    SpringBoot Init

--- backend/fivault/src/main/java/com/fivault/fivault/FivaultApplication.java
+++ backend/fivault/src/main/java/com/fivault/fivault/FivaultApplication.java
@@ -2,12 +2,13 @@ package com.fivault.fivault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class FivaultApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FivaultApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(FivaultApplication.class, args);
 	}
 
 }
