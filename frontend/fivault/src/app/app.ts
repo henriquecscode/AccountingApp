a348032cfb377098c9c587306a1d00d607a4aeb2
henriquecscode <henriquecostasousa.hcs@gmail.com>
2026-01-04 12:40:14 +0000

    navbar, header, footer, home

--- frontend/fivault/src/app/app.ts
+++ frontend/fivault/src/app/app.ts
@@ -1,9 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './layout/navbar/navbar';
+import { Footer } from './layout/footer/footer';
+import { Header } from './layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Navbar,
+    Footer,
+    Header
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
